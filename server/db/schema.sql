a956e66eed1cc987a7ae8b26b314872cbb60a812
Andy Bettisworth <andy@accreu.com>
2019-06-20 17:32:56 -0500

    Run migration create_posts

--- server/db/schema.sql
+++ server/db/schema.sql
@@ -1,5 +1,11 @@
 BEGIN TRANSACTION;
 
-
+CREATE TABLE `posts` (
+  `id` integer not null primary key autoincrement,
+  `title` text not null,
+  `content` text,
+  `created_at` datetime not null default CURRENT_TIMESTAMP,
+  `updated_at` datetime not null default CURRENT_TIMESTAMP
+);
 
 COMMIT TRANSACTION;
